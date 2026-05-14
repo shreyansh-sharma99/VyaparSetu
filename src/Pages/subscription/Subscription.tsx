32661a249c2e46ff8c87fa1794a6622adaffc3a8
Shreyansh Sharma <shreyanshsharma092@gmail.com>
2026-05-15 00:01:26 +0530

    refactor: Remove unused useState import in Subscription component

--- src/Pages/subscription/Subscription.tsx
+++ src/Pages/subscription/Subscription.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "../../store";
 import { fetchSubscriptions, setSearchQuery, setStatusFilter, setTenureFilter, setPagination } from "./services/subscriptionSlice";
